102fefcdea184c91b38991ec6f834d1231c79a0b
Pietro Gagliardi <pietro10@mac.com>
2014-06-21 23:10:31 -0400

    Fixed and verified the Go code as functional.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"unsafe"
 )
 
 // #cgo pkg-config: gobject-introspection-1.0
@@ -15,11 +16,15 @@ var (
 	gtkns = &_gtkns[0]
 )
 
+func fromgstr(str *C.gchar) string {
+	return C.GoString((*C.char)(unsafe.Pointer(str)))
+}
+
 func main() {
 	var err *C.GError = nil
 
 	gtk := C.g_irepository_require(nil, gtkns, nil, 0, &err)
-	if (gtk == NULL) {
+	if gtk == nil {
 		fmt.Fprintf(os.Stderr, "error opening GTK+ gi repository: %s", err.message)
 		os.Exit(1)
 	}
@@ -28,7 +33,7 @@ func main() {
 		info := C.g_irepository_get_info(nil, gtkns, i)
 		fmt.Printf("type:%d name:%s\n",
 			C.g_base_info_get_type(info),
-			C.GoString(C.g_base_info_get_name(info)))
+			fromgstr(C.g_base_info_get_name(info)))
 		C.g_base_info_unref(info);
 	}
 }
