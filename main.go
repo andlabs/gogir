ed921871de4e2b030bb1ef723b805b1c49c9c4a8
Pietro Gagliardi <pietro10@mac.com>
2014-06-21 23:28:20 -0400

    Added more descriptive information about each item.

--- main.go
+++ main.go
@@ -11,6 +11,29 @@ import (
 // #include <girepository.h>
 import "C"
 
+var typenames = map[C.GIInfoType]string{
+	C.GI_INFO_TYPE_INVALID:		"invalid",
+	C.GI_INFO_TYPE_FUNCTION:		"function",
+	C.GI_INFO_TYPE_CALLBACK:		"callback",
+	C.GI_INFO_TYPE_STRUCT:		"struct",
+	C.GI_INFO_TYPE_BOXED:			"boxed",
+	C.GI_INFO_TYPE_ENUM:			"enum",
+	C.GI_INFO_TYPE_FLAGS:			"flags",
+	C.GI_INFO_TYPE_OBJECT:		"object",
+	C.GI_INFO_TYPE_INTERFACE:		"interface",
+	C.GI_INFO_TYPE_CONSTANT:		"constant",
+	C.GI_INFO_TYPE_INVALID_0:		"invalid0",
+	C.GI_INFO_TYPE_UNION:			"union",
+	C.GI_INFO_TYPE_VALUE:			"value",
+	C.GI_INFO_TYPE_SIGNAL:		"signal",
+	C.GI_INFO_TYPE_VFUNC:			"vfunc",
+	C.GI_INFO_TYPE_PROPERTY:		"property",
+	C.GI_INFO_TYPE_FIELD:			"field",
+	C.GI_INFO_TYPE_ARG:			"arg",
+	C.GI_INFO_TYPE_TYPE:			"type",
+	C.GI_INFO_TYPE_UNRESOLVED:	"unresolved",
+}
+
 var (
 	_gtkns = [...]C.gchar{'G', 't', 'k', 0}
 	gtkns = &_gtkns[0]
@@ -30,10 +53,17 @@ func main() {
 	}
 	ninfo := C.g_irepository_get_n_infos(nil, gtkns)
 	for i := C.gint(0); i < ninfo; i++ {
+		var iter C.GIAttributeIter		// sets first member (really all members) to zero
+		var name, value *C.char
+
 		info := C.g_irepository_get_info(nil, gtkns, i)
-		fmt.Printf("type:%d name:%s\n",
-			C.g_base_info_get_type(info),
+		fmt.Printf("%s %s attribs { \n",
+			typenames[C.g_base_info_get_type(info)],
 			fromgstr(C.g_base_info_get_name(info)))
+		for C.g_base_info_iterate_attributes(info, &iter, &name, &value) != C.FALSE {
+			fmt.Printf("\t%q = %q,\n", C.GoString(name), C.GoString(value))
+		}
+		fmt.Printf("}\n")
 		C.g_base_info_unref(info);
 	}
 }
