9a27f8bb98b62b79b03e618a2cf3c8200b19aa82
Pietro Gagliardi <pietro10@mac.com>
2014-06-23 00:49:57 -0400

    Made the repo checker more elaborate as I installed more repos.

--- allrepos.sh
+++ allrepos.sh
@@ -1,7 +1,29 @@
 set -e
 # JSCore is being dumb so omit that for now (TODO)
 # win32 doesn't exist as a repo?! omit that too for now (TODO)
-ls /usr/share/gir-1.0/ | sed 's/-/ /g' | egrep -v 'JSCore|win32' | sed 's/\.gir$//g' | while read -r repo ver; do
-	echo $repo $ver
-	G_DEBUG=fatal-warnings ./gogir $repo $ver > /dev/null
-done
+# AppStream fails with ** (process:5348): CRITICAL **: g_irepository_find_by_name: assertion 'typelib != NULL' failed despite the typelib being there (TODO)
+# BraseroBurn has a version mismatch (TODO)
+# FolksEds and FolksTelepathy also have the typelib != NULL failure (TODO; TODO are the typelibs there?)
+# Grip and Hud complain about the typeinfo not being found despite having the packages installed (TODO)
+# UnityExtras: see FolksEds above (TODO)
+# Urfkill: see Grip above
+ls /usr/share/gir-1.0/ | sed 's/-/ /g' |
+	egrep -v 'JSCore|win32|AppStream|BraseroBurn|FolksEds|FolksTelepathy|Grip|Hud|UnityExtras|Urfkill' |
+	sed 's/\.gir$//g' | while read -r repo ver; do
+		echo $repo $ver
+		G_DEBUG=fatal-warnings ./gogir $repo $ver > /dev/null
+	done
+
+# some girs I would not install due to unwanted or unverified dependencies:
+# - emscritem (wants to pull in java)
+# - ibus-anthy-dev (wants to pull in ibus)
+# - libaccount-plugin-1.0-dev (wants to pull in signond)
+# - libcryptui-dev (wants to pull in seahorse-daemon)
+# - libfriends-dev, libfriends-gtk-dev (want to pull in friends-dispatcher)
+# - libguestfs-gobject-dev (wants to pull in a lot of things, including btrfs)
+# - libmuffin-dev (wants to pull in muffin-common)
+# - libsignon-glib-dev (wants to pull in signond)
+# - libskk-dev (wants to pull in skkdic)
+# - libunity-webapps-dev (wants to pull in a bunch of unity stuff)
+# - ubiquity-frontend-gtk (not a problem itself; pulls in a bunch of stuff I don't know what it is and would need to check)
+# this may change in the future
