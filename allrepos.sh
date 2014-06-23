54693573194451d6e8f142540a5c5a00c09c561a
Pietro Gagliardi <pietro10@mac.com>
2014-06-22 23:16:35 -0400

    Disabled two problematic repos for now. All the ones I have installed now pass!

--- allrepos.sh
+++ allrepos.sh
@@ -1,5 +1,7 @@
 set -e
-ls /usr/share/gir-1.0/ | sed 's/-/ /g' | sed 's/\.gir$//g' | while read -r repo ver; do
+# JSCore is being dumb so omit that for now (TODO)
+# win32 doesn't exist as a repo?! omit that too for now (TODO)
+ls /usr/share/gir-1.0/ | sed 's/-/ /g' | egrep -v 'JSCore|win32' | sed 's/\.gir$//g' | while read -r repo ver; do
 	echo $repo $ver
 	G_DEBUG=fatal-warnings ./gogir $repo $ver > /dev/null
 done
